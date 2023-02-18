31a1f07404fd913dd1ada2eb1854b536b16af91a
Robert Castles <rcastles@gmail.com>
2023-02-18 16:24:48 -0500

    some guesswork, an example js error page

--- code-to-inject.js
+++ code-to-inject.js
@@ -1,111 +1,114 @@
-// alert('code-to-inject');
-console.log('code-to-inject 2')
+;(() => {
+  // alert('code-to-inject');
+  console.log('code-to-inject loaded')
+  alert('cti')
+  console.log(window)
+  // function codeToInject() {
 
-// function codeToInject() {
+  function handleCustomError(message, stack) {
+    console.log('handleCustomError')
+    if (!stack) {
+      stack = new Error().stack.split('\n').splice(2, 4).join('\n')
+    }
 
+    var stackLines = stack.split('\n')
+    var callSrc = (stackLines.length > 1 &&
+      (/^.*?\((.*?):(\d+):(\d+)/.exec(stackLines[1]) ||
+        /(\w+:\/\/.*?):(\d+):(\d+)/.exec(stackLines[1]))) || [
+      null,
+      null,
+      null,
+      null,
+    ]
 
-function handleCustomError(message, stack) {
-  console.log('handleCustomError')
-  if (!stack) {
-    stack = new Error().stack.split('\n').splice(2, 4).join('\n')
-  }
-
-  var stackLines = stack.split('\n')
-  var callSrc = (stackLines.length > 1 &&
-    (/^.*?\((.*?):(\d+):(\d+)/.exec(stackLines[1]) ||
-      /(\w+:\/\/.*?):(\d+):(\d+)/.exec(stackLines[1]))) || [
-    null,
-    null,
-    null,
-    null,
-  ]
-
-  document.dispatchEvent(
-    new CustomEvent('ErrorToExtension', {
-      detail: {
-        stack: stackLines.join('\n'),
-        url: callSrc[1],
-        line: callSrc[2],
-        col: callSrc[3],
-        text: message,
-      },
-    }),
-  )
-}
-
-console.log('cti try addEventListener')
-
-// debugger;
-
-// handle uncaught promises errors
-window.addEventListener('unhandledrejection', function (e) {
-	console.log('unhandledrejection')
-	this.alert('unhandledrejection')
-
-  if (typeof e.reason === 'undefined') {
-    e.reason = e.detail
-  }
-  handleCustomError(e.reason.message, e.reason.stack)
-})
-
-// handle console.error()
-var consoleErrorFunc = window.console.error
-window.console.error = function () {
-	  console.log('window.console.error function')
-this.alert('window.console.error')
-  var argsArray = []
-  for (var i in arguments) {
-    // because arguments.join() not working! oO
-    argsArray.push(arguments[i])
-  }
-  consoleErrorFunc.apply(console, argsArray)
-
-  handleCustomError(
-    argsArray.length == 1 && typeof argsArray[0] == 'string'
-      ? argsArray[0]
-      : JSON.stringify(argsArray.length == 1 ? argsArray[0] : argsArray),
-  )
-}
-
-// handle uncaught errors
-window.addEventListener('error', function (e) {
-		  console.log('event listener error')
-this.alert('cti error event')
-  if (e.filename) {
     document.dispatchEvent(
       new CustomEvent('ErrorToExtension', {
         detail: {
-          stack: e.error ? e.error.stack : null,
-          url: e.filename,
-          line: e.lineno,
-          col: e.colno,
-          text: e.message,
+          stack: stackLines.join('\n'),
+          url: callSrc[1],
+          line: callSrc[2],
+          col: callSrc[3],
+          text: message,
         },
       }),
     )
   }
-})
 
-// handle 404 errors
-window.addEventListener(
-  'error',
-	function (e) {
-		console.log('listener ErrorEvent')
-		this.alert('cti error thing')
+  console.log('cti try addEventListener')
 
-    var src = e.target.src || e.target.href
-    var baseUrl = e.target.baseURI
-    if (src && baseUrl && src != baseUrl) {
+  // debugger;
+
+  // handle uncaught promises errors
+  window.addEventListener('unhandledrejection', function (e) {
+    console.log('code-to-inject eventListener unhandledrejection')
+    this.alert('code-to-inject eventListener unhandledrejection')
+
+    if (typeof e.reason === 'undefined') {
+      e.reason = e.detail
+    }
+    handleCustomError(e.reason.message, e.reason.stack)
+  })
+
+  // handle console.error()
+  var consoleErrorFunc = window.console.error
+  window.console.error = function () {
+    console.log('window.console.error function')
+    this.alert('window.console.error')
+    var argsArray = []
+    for (var i in arguments) {
+      // because arguments.join() not working! oO
+      argsArray.push(arguments[i])
+    }
+    consoleErrorFunc.apply(console, argsArray)
+
+    handleCustomError(
+      argsArray.length == 1 && typeof argsArray[0] == 'string'
+        ? argsArray[0]
+        : JSON.stringify(argsArray.length == 1 ? argsArray[0] : argsArray),
+    )
+  }
+  // handle uncaught errors
+  window.addEventListener('error', function (e) {
+    console.log('cti event listener error')
+    this.alert('cti error event')
+    if (e.filename) {
       document.dispatchEvent(
         new CustomEvent('ErrorToExtension', {
           detail: {
-            is404: true,
-            url: src,
+            stack: e.error ? e.error.stack : null,
+            url: e.filename,
+            line: e.lineno,
+            col: e.colno,
+            text: e.message,
           },
         }),
       )
     }
-  },
-  true,
-)
-// }
+  })
+
+  // handle 404 errors
+  window.addEventListener(
+    'error',
+    function (e) {
+      console.log('listener ErrorEvent')
+      this.alert('cti error thing')
+
+      var src = e.target.src || e.target.href
+      var baseUrl = e.target.baseURI
+      if (src && baseUrl && src != baseUrl) {
+        document.dispatchEvent(
+          new CustomEvent('ErrorToExtension', {
+            detail: {
+              is404: true,
+              url: src,
+            },
+          }),
+        )
+      }
+    },
+    true,
+  )
+
+  console.log('end of cti')
+  // }
+})()
--- content.js
+++ content.js
@@ -1,111 +1,181 @@
-new function() {
+new (function () {
+  var errors = []
+  var errorsLimit = 100
+  var tabId
+  var timer
+  var icon
+  var popup
+  var options
+  var isIFrame = window.top != window
 
-	var errors = [];
-	var errorsLimit = 100;
-	var tabId;
-	var timer;
-	var icon;
-	var popup;
-	var options;
-	var isIFrame = window.top != window;
+  // alert('content.js')
+  console.log('content.js')
 
-	// alert('content.js')
-	console.log('content.js');
+  // sw.evaluate(() => chrome.storage.local.write('key', 'value'))
 
-	// sw.evaluate(() => chrome.storage.local.write('key', 'value'))
+  function showPopup(popupUrl) {
+    if (!popup) {
+      popup = document.createElement('iframe')
+      popup.src = popupUrl
+      popup.frameBorder = 0
+      popup.style.cssText =
+        'position: fixed !important; bottom: 50px !important; right: 50px !important; z-index: 2147483647 !important;'
+      popup.height = '50px'
+      ;(document.body || document.documentElement).appendChild(popup)
+    } else {
+      popup.contentWindow.postMessage(
+        {
+          _reloadPopup: true,
+          url: popupUrl,
+        },
+        '*',
+      )
+    }
+  }
 
-	function showPopup(popupUrl) {
-		if(!popup) {
-			popup = document.createElement('iframe');
-			popup.src = popupUrl;
-			popup.frameBorder = 0;
-			popup.style.cssText = 'position: fixed !important; bottom: 50px !important; right: 50px !important; z-index: 2147483647 !important;';
-			popup.height = '50px';
-			(document.body || document.documentElement).appendChild(popup);
-		}
-		else {
-			popup.contentWindow.postMessage({
-				_reloadPopup: true,
-				url: popupUrl
-			}, '*');
-		}
-	}
+  function showErrorNotification(popupUrl) {
+    if (options.showPopup) {
+      showPopup(popupUrl)
+    }
 
-	function showErrorNotification(popupUrl) {
-		if(options.showPopup) {
-			showPopup(popupUrl);
-		}
+    if (!icon && (options.showIcon || options.showPopup)) {
+      icon = document.createElement('img')
+      icon.src = chrome.extension.getURL('img/error_38.png')
+      icon.title = 'Some errors occurred on this page. Click to see details.'
+      icon.style.cssText =
+        'position: fixed !important; bottom: 10px !important; right: 10px !important; cursor: pointer !important; z-index: 2147483647 !important; width: 38px !important; height: 38px !important; min-height: 38px !important; min-width: 38px !important; max-height: 38px !important; max-width: 38px !important;'
+      icon.onclick = function () {
+        if (!popup) {
+          showPopup(popupUrl)
+        } else {
+          popup.remove()
+          popup = null
+        }
+      }
+      if (options.showPopupOnMouseOver) {
+        icon.onmouseover = function () {
+          if (!popup) {
+            showPopup(popupUrl)
+          }
+        }
+      }
+      ;(document.body || document.documentElement).appendChild(icon)
+    }
+  }
 
-		if(!icon && (options.showIcon || options.showPopup)) {
-			icon = document.createElement('img');
-			icon.src = chrome.extension.getURL('img/error_38.png');
-			icon.title = 'Some errors occurred on this page. Click to see details.';
-			icon.style.cssText = 'position: fixed !important; bottom: 10px !important; right: 10px !important; cursor: pointer !important; z-index: 2147483647 !important; width: 38px !important; height: 38px !important; min-height: 38px !important; min-width: 38px !important; max-height: 38px !important; max-width: 38px !important;';
-			icon.onclick = function() {
-				if(!popup) {
-					showPopup(popupUrl);
-				}
-				else {
-					popup.remove();
-					popup = null;
-				}
-			};
-			if(options.showPopupOnMouseOver) {
-				icon.onmouseover = function() {
-					if(!popup) {
-						showPopup(popupUrl);
-					}
-				};
-			}
-			(document.body || document.documentElement).appendChild(icon);
-		}
-	}
+  function handleNewError(error) {
+    alert('handleNewError')
+    var lastError = errors[errors.length - 1]
+    var isSameAsLast =
+      lastError &&
+      lastError.text == error.text &&
+      lastError.url == error.url &&
+      lastError.line == error.line &&
+      lastError.col == error.col
+    var isWrongUrl = !error.url || error.url.indexOf('://') === -1
+    if (!isSameAsLast && !isWrongUrl) {
+      errors.push(error)
+      if (errors.length > errorsLimit) {
+        errors.shift()
+      }
+      if (!timer) {
+        timer = window.setTimeout(function () {
+          timer = null
+          chrome.runtime.sendMessage(
+            {
+              _errors: true,
+              errors: errors,
+              url: window.top.location.href,
+            },
+            function (popupUrl) {
+              if (popupUrl) {
+                showErrorNotification(popupUrl)
+              }
+            },
+          )
+        }, 200)
+      }
+    }
+  }
 
-	function handleNewError(error) {
-		var lastError = errors[errors.length - 1];
-		var isSameAsLast = lastError && lastError.text == error.text && lastError.url == error.url && lastError.line == error.line && lastError.col == error.col;
-		var isWrongUrl = !error.url || error.url.indexOf('://') === -1;
-		if(!isSameAsLast && !isWrongUrl) {
-			errors.push(error);
-			if(errors.length > errorsLimit) {
-				errors.shift();
-			}
-			if(!timer) {
-				timer = window.setTimeout(function() {
-					timer = null;
-					chrome.runtime.sendMessage({
-						_errors: true,
-						errors: errors,
-						url: window.top.location.href
-					}, function(popupUrl) {
-						if(popupUrl) {
-							showErrorNotification(popupUrl);
-						}
-					});
-				}, 200);
-			}
-		}
-	}
+  document.addEventListener('ErrorToExtension', function (e) {
+    var error = e.detail
+    if (isIFrame) {
+      window.top.postMessage(
+        {
+          _iframeError: true,
+          _fromJEN: true,
+          error: error,
+        },
+        '*',
+      )
+    } else {
+      handleNewError(error)
+    }
+  })
 
-	document.addEventListener('ErrorToExtension', function(e) {
-		var error = e.detail;
-		if(isIFrame) {
-			window.top.postMessage({
-				_iframeError: true,
-				_fromJEN: true,
-				error: error
-			}, '*');
-		}
-		else {
-			handleNewError(error);
-		}
-	});	
+  function handleInternalMessage(data) {
+    if (!isIFrame && (!data.tabId || data.tabId == tabId)) {
+      if (data._clear) {
+        errors = []
+        if (popup) {
+          popup.remove()
+          popup = null
+        }
+        if (icon) {
+          icon.remove()
+          icon = null
+        }
+      } else if (data._resize && popup) {
+        var maxHeight =
+          Math.round((window.innerHeight * options.popupMaxHeight) / 100) - 60
+        var maxWidth =
+          Math.round((window.innerWidth * options.popupMaxWidth) / 100) - 60
+        var height = data.height < maxHeight ? data.height : maxHeight
+        var width = data.width < maxWidth ? data.width : maxWidth
+        popup.height = (width == maxWidth ? height + 10 : height) + 'px' // scroll fix
+        popup.width = (height == maxHeight ? width + 10 : width) + 'px' // scroll fix
+        popup.style.height = popup.height
+        popup.style.width = popup.width
+      } else if (data._closePopup && popup) {
+        popup.style.display = 'none'
+      } else if (data._iframeError) {
+        handleNewError(data.error)
+      }
+    }
+  }
 
-	// _script = document.createElement('script');
-	// _script.setAttribute('src', chrome.runtime.getURL('code-to-inject.js'));
-	// (document.head||document.documentElement).appendChild( _script  );
-	// _script.parentNode.removeChild(_script);
-	
+  chrome.runtime.onMessage.addListener(handleInternalMessage)
+
+  window.addEventListener('message', function (event) {
+    if (
+      typeof event.data === 'object' &&
+      event.data &&
+      typeof event.data._fromJEN !== 'undefined' &&
+      event.data._fromJEN
+    ) {
+      handleInternalMessage(event.data)
+    }
+  })
+
+  if (!isIFrame) {
+    chrome.runtime.sendMessage(
+      {
+        _initPage: true,
+        url: window.location.href,
+      },
+      function (response) {
+        options = response
+      },
+    )
+  }
+})()
+
+// _script = document.createElement('script');
+// _script.setAttribute('src', chrome.runtime.getURL('code-to-inject.js'));
+// (document.head||document.documentElement).appendChild( _script  );
+// _script.parentNode.removeChild(_script);
+
 // var s = document.createElement('script');
 // 	s.src = "code-to-inject.js"
 // 		chrome.runtime.getURL('code-to-inject.js');
@@ -113,191 +183,6 @@ new function() {
 //     this.remove();
 // };
 // (document.head || document.documentElement).appendChild(s);
-	
-	function handleInternalMessage(data) {
-		if(!isIFrame && (!data.tabId || data.tabId == tabId)) {
-			if(data._clear) {
-				errors = [];
-				if(popup) {
-					popup.remove();
-					popup = null;
-				}
-				if(icon) {
-					icon.remove();
-					icon = null;
-				}
-			}
-			else if(data._resize && popup) {
-				var maxHeight = Math.round(window.innerHeight * options.popupMaxHeight / 100) - 60;
-				var maxWidth = Math.round(window.innerWidth * options.popupMaxWidth / 100) - 60;
-				var height = data.height < maxHeight ? data.height : maxHeight;
-				var width = data.width < maxWidth ? data.width : maxWidth;
-				popup.height = (width == maxWidth ? height + 10 : height) + 'px'; // scroll fix
-				popup.width = (height == maxHeight ? width + 10 : width) + 'px'; // scroll fix
-				popup.style.height = popup.height;
-				popup.style.width = popup.width;
-			}
-			else if(data._closePopup && popup) {
-				popup.style.display = 'none';
-			}
-			else if(data._iframeError) {
-				handleNewError(data.error);
-			}
-		}
-	}
 
-	chrome.runtime.onMessage.addListener(handleInternalMessage);
-
-	window.addEventListener('message', function(event) {
-		if(typeof event.data === 'object' && event.data && typeof event.data._fromJEN !== 'undefined' && event.data._fromJEN) {
-			handleInternalMessage(event.data);
-		}
-	});
-
-	if(!isIFrame) {
-		chrome.runtime.sendMessage({
-			_initPage: true,
-			url: window.location.href
-		}, function(response) {
-			options = response;
-		});
-	}
-
-
-	// alert('code-to-inject');
+// alert('code-to-inject');
 // function codeToInject() {
-
-	console.log('content.js error handlers')
-	
-function handleCustomError(message, stack) {
-  console.log('handleCustomError')
-  if (!stack) {
-    stack = new Error().stack.split('\n').splice(2, 4).join('\n')
-  }
-
-  var stackLines = stack.split('\n')
-  var callSrc = (stackLines.length > 1 &&
-    (/^.*?\((.*?):(\d+):(\d+)/.exec(stackLines[1]) ||
-      /(\w+:\/\/.*?):(\d+):(\d+)/.exec(stackLines[1]))) || [
-    null,
-    null,
-    null,
-    null,
-  ]
-
-  document.dispatchEvent(
-    new CustomEvent('ErrorToExtension', {
-      detail: {
-        stack: stackLines.join('\n'),
-        url: callSrc[1],
-        line: callSrc[2],
-        col: callSrc[3],
-        text: message,
-      },
-    }),
-  )
-}
-
-console.log('content.js try addEventListener')
-
-// debugger;
-
-// handle uncaught promises errors
-window.addEventListener('unhandledrejection', function (e) {
-	console.log('unhandledrejection')
-	debugger
-	// this.alert('unhandledrejection')
-
-  if (typeof e.reason === 'undefined') {
-    e.reason = e.detail
-  }
-  handleCustomError(e.reason.message, e.reason.stack)
-})
-
-// handle console.error()
-var consoleErrorFunc = window.console.error
-window.console.error = function () {
-	console.log('window.console.error function')
-	debugger
-// this.alert('window.console.error')
-  var argsArray = []
-  for (var i in arguments) {
-    // because arguments.join() not working! oO
-    argsArray.push(arguments[i])
-  }
-  consoleErrorFunc.apply(console, argsArray)
-
-  handleCustomError(
-    argsArray.length == 1 && typeof argsArray[0] == 'string'
-      ? argsArray[0]
-      : JSON.stringify(argsArray.length == 1 ? argsArray[0] : argsArray),
-  )
-}
-
-	
-// handle uncaught errors
-document.addEventListener('error', function (e) {
-	console.log('event listener error')
-	debugger
-// this.alert('content.js error event')
-  if (e.filename) {
-    document.dispatchEvent(
-      new CustomEvent('ErrorToExtension', {
-        detail: {
-          stack: e.error ? e.error.stack : null,
-          url: e.filename,
-          line: e.lineno,
-          col: e.colno,
-          text: e.message,
-        },
-      }),
-    )
-  }
-})
-	
-// handle uncaught errors
-window.addEventListener('error', function (e) {
-	console.log('event listener error')
-	debugger
-// this.alert('content.js error event')
-  if (e.filename) {
-    document.dispatchEvent(
-      new CustomEvent('ErrorToExtension', {
-        detail: {
-          stack: e.error ? e.error.stack : null,
-          url: e.filename,
-          line: e.lineno,
-          col: e.colno,
-          text: e.message,
-        },
-      }),
-    )
-  }
-})
-
-// handle 404 errors
-window.addEventListener(
-  'error',
-	function (e) {
-		console.log('listener ErrorEvent')
-		debugger
-		// this.alert('content.js error thing')
-
-    var src = e.target.src || e.target.href
-    var baseUrl = e.target.baseURI
-    if (src && baseUrl && src != baseUrl) {
-      document.dispatchEvent(
-        new CustomEvent('ErrorToExtension', {
-          detail: {
-            is404: true,
-            url: src,
-          },
-        }),
-      )
-    }
-  },
-  true,
-)
-// }
-
-};
